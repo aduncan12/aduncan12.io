3d571ce4c44a42ec799ca8e088439c05dd376fa7
Alan <a386226@gmail.com>
2018-12-11 08:44:49 -0800

    updated site

--- js/main.js
+++ js/main.js
@@ -5,51 +5,66 @@ for(i = 0; i < mainDivs.length; i++) {
     zFlyers[i] = $(mainDivs[i]).css("z-index");
 }
 
+// var flyerAnimate = 
+//     $('.aboutFlyer').css("width", "60vh")
+//     $('.aboutFlyer').css("height", "95vh")
+//     $('.aboutFlyer').css("transform", "0deg")
+
 // button links to flyers 
 $('#about').on('click', function() {
-    $('.about').css("z-index", zFlyers.length++)
-    $('.about').toggleClass('aboutClicked');
+    $('.aboutFlyer').css("z-index", zFlyers.length++)
+    $('.aboutFlyer').addClass('allClicked')
+    $('.projectsFlyer').removeClass('allClicked')
+    $('.testFlyer').removeClass('allClicked')
+    $('.contactFlyer').removeClass('allClicked');
 });
 
 $('#aboutNav').on('click', function() {
-    $('.about').css("z-index", zFlyers.length++);
+    $('.aboutFlyer').css("z-index", zFlyers.length++);
 });
 
 $('#projects').on('click', function() {
-    $('.projects').css("z-index", zFlyers.length++)
-    $('.projects').toggleClass('projectsClicked');
+    $('.projectsFlyer').css("z-index", zFlyers.length++)
+    $('.projectsFlyer').addClass('allClicked')
+    $('.aboutFlyer').removeClass('allClicked')
+    $('.testFlyer').removeClass('allClicked')
+    $('.contactFlyer').removeClass('allClicked');
 });
 
 $('#projectsNav').on('click', function() {
-    $('.projects').css("z-index", zFlyers.length++);
+    $('.projectsFlyer').css("z-index", zFlyers.length++);
 });
 
 $('#testimonials').on('click', function() {
-    $('.testimonials').css("z-index", zFlyers.length++)
-    $('.testimonials').toggleClass('testimonialsClicked');
+    $('.testFlyer').css("z-index", zFlyers.length++)
+    $('.testFlyer').addClass('allClicked')
+    $('.aboutFlyer').removeClass('allClicked')
+    $('.projectsFlyer').removeClass('allClicked')
+    $('.contactFlyer').removeClass('allClicked');
 });
 
 $('#testimonialsNav').on('click', function() {
-    $('.testimonials').css("z-index", zFlyers.length++);
+    $('.testFlyer').css("z-index", zFlyers.length++);
 });
 
 $('#contact').on('click', function() {
-    $('.contact').css("z-index", zFlyers.length++)
-    $('.contact').toggleClass('contactClicked');
+    $('.contactFlyer').css("z-index", zFlyers.length++)
+    $('.contactFlyer').addClass('allClicked')
+    $('.aboutFlyer').removeClass('allClicked')
+    $('.projectsFlyer').removeClass('allClicked')
+    $('.testFlyer').removeClass('allClicked');
 });
 
 $('#contactNav').on('click', function() {
-    $('.contact').css("z-index", zFlyers.length++);
+    $('.contactFlyer').css("z-index", zFlyers.length++);
 });
-// button links to flyers 
 
 // testimonials 
 setInterval(function() {
     $('#test1').fadeIn(3500).fadeOut(3500, function () {
         $('#test2').fadeIn(3500).fadeOut(3500, function () {
-             $('#test3').fadeIn(3500).fadeOut(3500)            
+            $('#test3').fadeIn(3500).fadeOut(3500)            
         })
     })
 }, 21000);
-// testimonials
 
